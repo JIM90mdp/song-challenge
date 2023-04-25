d2b6c09ee762d158f0410cad9d39db302bc694fd
JIM90mdp <juanignaciomascarenhas@gmail.com>
2023-04-25 17:04:00 -0300

    Refactor/loder-spiner-managment

--- web-app/web-app/pages/_app.js
+++ web-app/web-app/pages/_app.js
@@ -1,13 +1,33 @@
 import "@component/styles/globals.css";
 import { Montserrat } from "@next/font/google";
 import MainLayout from "@component/components/layout/main-layout";
+import ReactLoading from "react-loading";
+import { useEffect, useState } from "react";
+
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
+
   return (
     <div className={`${montserrat.className} `}>
       <MainLayout>
-        <Component {...pageProps} />
+        {isLoading ? (
+          <div className="relative flex flex-col items-center justify-center h-[90vh]">
+            <ReactLoading
+              type={"bars"}
+              color={"#2c3e50"}
+              height={50}
+              width={50}
+            />
+          </div>
+        ) : (
+          <Component {...pageProps} />
+        )}
       </MainLayout>
     </div>
   );
