9473d0cb02f7eb2ac201593fb3912ae8876595e2
JIM90mdp <juanignaciomascarenhas@gmail.com>
2023-11-20 17:24:58 -0300

    Del: api

--- challenge-app/next.config.js
+++ challenge-app/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: "export",
+  images: { unoptimized: true },
+  basePath: "/static",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
