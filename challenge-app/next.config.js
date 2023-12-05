c1f3f2a7032c23fed020c6733432077bd47dfe6d
JIM90mdp <juanignaciomascarenhas@gmail.com>
2023-12-05 16:34:43 -0300

    Comenté la linea basePath: '/static' en next.config

--- challenge-app/next.config.js
+++ challenge-app/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   output: "export",
   images: { unoptimized: true },
-  basePath: "/static",
+  // basePath: "/static",
 };
 
 module.exports = nextConfig;
